6db0687f0ad8ff274927ec01d511880fae1befbe
zhouxing <2579847301@qq.com>
2023-04-18 17:40:55 +0800

    second commit

---
+++ chapter6/sql/test.sql
@@ -0,0 +1,12 @@
+create table posts (
+                       id      serial primary key,
+                       content text,
+                       author  varchar(255)
+);
+
+create table comments (
+                          id      serial primary key,
+                          content text,
+                          author  varchar(255),
+                          post_id integer references posts(id)
+);
--- go.mod
+++ go.mod
@@ -1,3 +1,16 @@
 module Go_web_Programming_Code
 
 go 1.20
+
+require (
+	github.com/jinzhu/gorm v1.9.16
+	github.com/jmoiron/sqlx v1.3.5
+	github.com/julienschmidt/httprouter v1.3.0
+	github.com/lib/pq v1.10.8
+	golang.org/x/net v0.9.0
+)
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+)
